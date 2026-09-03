4161d0309564e402c493bc13a7dc8ef5e61fd47f
Navid882 <navid.behazin.288@gmail.com>
2026-09-03 15:28:43 +0300

    add templates for nodes

--- src/my_cpp_pkg/src/my_first_node.cpp
+++ src/my_cpp_pkg/src/my_first_node.cpp
@@ -1,8 +1,5 @@
 #include "rclcpp/rclcpp.hpp" 
 
-// ctrl+shif+p then c/c++ edit configuration json
-// then copy the path opt/ros/lyrical/include/** 
-// under iclude path section
 class MyNode: public rclcpp::Node
 {
     public:
@@ -28,11 +25,8 @@ int main (int argc, char **argv)
     auto node=std::make_shared<MyNode>();
     // auto node=std::make_shared<rclcpp::Node>("cpp_test");//make the node using shared pointer
     // RCLCPP_INFO(node->get_logger(), "Hello cpp node");//print sth
-    rclcpp::spin(node);//make alive
+    rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
 
 }
-// we need to copmile first then install it
-// go to cmakelist.txt
-// then we go this address ~/ros2_ws/install/my_cpp_pkg/lib/my_cpp_pkg and run the code ./cpp_node
